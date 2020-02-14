e89917a12ffae199ac0a66a0bf7e875086fb121b
AdvWeb <advweb@Admins-iMac-5.local>
2020-02-14 09:16:49 +0700

    test

--- vue-ticket/src/views/Home.vue
+++ vue-ticket/src/views/Home.vue
@@ -1,81 +1,18 @@
 <template>
   <div class="home">
-    <div class="form">
-      <div class="title">
-        <h1>TICKET</h1>
-      </div>
-      <div class="button">
-        <button @click="getTicket">
-          GET
-        </button>
-      </div>
-      <div class="link" @click="goToAbout"> 
-        Ticket Remind
-      </div>
-    </div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
-import {mapActions,mapGetters} from 'vuex';
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  computed:{
-    ...mapGetters({
-      ticket:'ticket'
-    })
-  },
-  methods:{
-    ...mapActions({
-      getTicketFromStore:'getTicketFromStore'
-    }),
-    getTicket(){
-      let ticketLeft = this.ticket - 1
-      this.getTicketFromStore(ticketLeft)
-      alert('You got ticket')
-    },
-    goToAbout(){
-      this.$router.push('./about')
-    }
+  components: {
+    HelloWorld
   }
 }
 </script>
-<style>
-  .home{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height:100% ;
-  }
-  .form{
-    width: 450px;
-    height: 300px;
-    background-color: aliceblue;
-    box-shadow: 1px 1px 25px 2px cornsilk;
-    text-align: center;
-  }
-  .title{
-    margin-top: 50px;
-  }
-  .button{
-    margin-top: 80px;
-  }
-  button{
-    outline: none;
-    cursor: pointer;
-    border: 1px solid black;
-    border-radius: 5px;
-    padding:10px 40px;
-  }      
-  .link{
-    margin-top:50px;
-    margin-right:10px;
-    text-align: right;
-    text-decoration: underline;
-    cursor: pointer;
-  }
-
-</style>
